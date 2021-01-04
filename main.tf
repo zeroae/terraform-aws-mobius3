7866255f6398355e1baecab64dd4d79e826f91c5
Patrick Sodré <psodre@gmail.com>
2021-01-04 11:44:03 -0500

    Initial codebase

--- main.tf
+++ main.tf
@@ -1,13 +1,86 @@
-resource "random_integer" "example" {
-  count = module.this.enabled ? 1 : 0
+locals {
+  key_prefix = trimsuffix(var.bucket_key_prefix, "/")
+}
 
-  min = 1
-  max = 50000
-  keepers = {
-    example = var.example
-  }
+module "volume_label" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
+  name        = var.volume_name
+  delimiter   = "_"
+  label_order = ["name"]
+}
+
+module "copy_from_s3" {
+  source          = "git::https://github.com/cloudposse/terraform-aws-ecs-container-definition.git?ref=tags/0.41.0"
+  container_name  = "copy_from_s3"
+  container_image = var.awscli_image
+  essential       = false
+
+  user = var.user
+
+  entrypoint = [
+    "/bin/sh"
+  ]
+  command = [
+    "-c",
+    join(";", [
+      "set -e",
+      "aws s3 sync s3://${var.bucket_id}/${local.key_prefix}/${module.volume_label.id}/ /srv/data",
+      "chown -R ${var.user} /srv/data",
+    ])
+  ]
+
+  mount_points = [
+    {
+      containerPath = "/srv/data"
+      sourceVolume  = module.volume_label.id
+    }
+  ]
+
+  log_configuration = var.log_configuration
+}
+
+# TODO: Implement a health check
+# TODO: Support multiple volumes
+module "mobius3" {
+  source          = "git::https://github.com/cloudposse/terraform-aws-ecs-container-definition.git?ref=tags/0.41.0"
+  container_name  = "mobius3"
+  container_image = var.mobius3_image
+  essential       = true
+
+  container_depends_on = [
+    {
+      containerName = module.copy_from_s3.json_map_object["name"]
+      condition     = "SUCCESS"
+    },
+  ]
+
+  user = var.user
+
+  command = [
+    "mobius3",
+    "/srv/data",
+    var.bucket_id,
+    "https://{}.s3.${var.bucket_region}.amazonaws.com/",
+    var.bucket_region,
+    "--prefix", "${local.key_prefix}/${module.volume_label.id}/",
+    "--credentials-source", "ecs-container-endpoint",
+    "--log-level", "INFO"
+  ]
+
+  mount_points = [
+    {
+      containerPath = "/srv/data"
+      sourceVolume  = module.volume_label.id
+    }
+  ]
+
+  log_configuration = var.log_configuration
 }
 
 locals {
-  example = format("%v %v", var.example, join("", random_integer.example[*].result))
+  output_container_depends_on = {
+    containerName = module.mobius3.json_map_object["name"]
+    # TODO: Use HEALTHY once we have a healthcheck
+    condition = "START"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,41 @@
-variable "example" {
-  description = "Example variable"
-  default     = "hello world"
+variable "bucket_id" {
+  description = "Bucket Name (aka ID)"
 }
+variable "bucket_region" {
+  description = "Bucket Region"
+}
+
+variable "bucket_key_prefix" {
+  description = "The key prefix to use for the bucket objects."
+}
+
+variable "volume_name" {
+  description = "The volume name."
+  default     = "data"
+}
+
+variable "user" {
+  type        = string
+  description = "The user that owns the volume. Can be any of these formats: uid, uid:gid. The default is (0:0)."
+  default     = "0:0"
+}
+
+variable "awscli_image" {
+  description = "The AWSCLI Compatible Image"
+  type        = string
+  default     = "docker.io/amazon/aws-cli:2.0.52"
+}
+
+variable "mobius3_image" {
+  description = "The Mobius3 Image for S3<->FS synchronization"
+  type        = string
+  default     = "quay.io/uktrade/mobius3:v0.0.32"
+}
+
+# https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_LogConfiguration.html
+variable "log_configuration" {
+  type        = any
+  description = "Log configuration options to send to a custom log driver for the container. For more details, see https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_LogConfiguration.html"
+  default     = null
+}
+
